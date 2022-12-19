ac020118045660b997e65f14387e9a3b0e3575af
coystark <chcp.caio@gmail.com>
2022-12-19 00:22:34 -0300

    fix chakra color mode flick

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  config: {
+    initialColorMode: "light",
+    useSystemColorMode: false,
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS>
+    <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- packages/frontend/src/pages/_document.tsx
+++ packages/frontend/src/pages/_document.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Html, Head, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
+import { theme } from "./_app";
 
 export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head />
       <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
